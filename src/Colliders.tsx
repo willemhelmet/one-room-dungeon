217756abf6c1f6445a12704ecfcf65706c71686c
Willem Helmet Pickleman <willemhelmet@gmail.com>
2025-12-02 14:10:10 -0500

    added unique key to static collider

--- src/Colliders.tsx
+++ src/Colliders.tsx
@@ -5,7 +5,7 @@ export const Colliders = () => {
   const gltf = useGLTF("teleportation-colliders.glb");
   return (
     <>
-      <StaticCollider>
+      <StaticCollider key={gltf.scene.uuid}>
         <group scale={[2, 2, 2]} rotation={[Math.PI * 0.5, 0, 0]}>
           <primitive object={gltf.scene} visible={false} />
         </group>
